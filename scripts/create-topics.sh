8939f23217db323bf1eb5f704057f53f308e9cac
Vinicius Komninakis <vinicius_k2013@hotmail.com>
2021-05-29 20:59:28 -0300

    feat: add basic scripts

---
+++ scripts/create-topics.sh
@@ -0,0 +1,20 @@
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+    --create \
+    --zookeeper zookeeper:2181 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic product-ordered
+
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+    --create \
+    --zookeeper zookeeper:2181 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic product-viewed
+
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+    --create \
+    --zookeeper zookeeper:2181 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic product-favorited
