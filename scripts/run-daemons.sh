d2668e8ccd7493afebbc6aeb5210de809c7f6a89
Vinicius Komninakis <vinicius_k2013@hotmail.com>
2021-06-02 03:03:14 -0300

    chore: fix daemons sh

--- scripts/run-daemons.sh
+++ scripts/run-daemons.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-dir="/Users/komninakis/Projects/Fiap/Microservices/microservices-fiap/microservices"
-
-cd $dir"/Fiap-order-services" && docker-compose up -d mysql nodejs
-cd $dir"/Fiap-favorites-services" && docker-compose up -d mysql nodejs
-cd $dir"/Fiap-product-services" && docker-compose up -d mysql nodejs
-cd $dir"/Fiap-report-services" && docker-compose up -d mysql nodejs
+cd $(pwd)"/microservices/Fiap-order-services" && docker-compose up --build -d mysql nodejs && cd ../..
+cd $(pwd)"/microservices/Fiap-favorites-services" && docker-compose up --build -d mysql nodejs && cd ../..
+cd $(pwd)"/microservices/Fiap-product-services" && docker-compose up --build -d mysql nodejs && cd ../..
+cd $(pwd)"/microservices/Fiap-report-services" && docker-compose up --build -d mysql nodejs && cd ../..
+cd $(pwd)"/microservices/Fiap-shipping-services" && docker-compose up --build -d mysql nodejs && cd ../../
